951728a6951d17b75b162afd31838be629bec26c
dhara-maru <dharamaru406@gmail.com>
2024-10-02 12:04:34 +0530

    API View Decorators

--- core/home/views.py
+++ core/home/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET', 'POST', 'PUT'])   #this func can have these methods.
+def index(request):
+    courses = {
+    "course_name": "Python",
+    "learn": ["Django", "Flask", "Tornado", "FastAPI"],
+    "course_provider": "Scaler"
+    }
+    if request.method == 'GET':
+        coursesapp = {
+        "course_name": "App Development",
+        "learn": ["Flutter", "Android", "FlutterFlow"],
+        "course_provider": "Coursera"
+        }
+        return Response(coursesapp)
+    elif request.method == 'POST':
+        return Response(courses)
